d762682085b576f5cd158840dc90f0e3fc1788f6
Satyam <satyampote9999@gmail.com>
2025-06-14 19:53:35 +0530

    add new files

--- movie_tracker/tracker/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('query/', views.query_like_count, name='query_like_count'), # added the path
-    path('export/', views.export_movie_csv, name='export_movie_csv'),  # Keep this
-]
